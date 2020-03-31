ef53cb4b0bf7cb89c6cfca324481a8a5ed8a8b0e
WeRockStar <kotchaphan.m@gmail.com>
2020-03-31 15:46:25 +0700

    Add CoreFramework

---
+++ CoreFramework/CoreFramework/CoreFramework.h
@@ -0,0 +1,19 @@
+//
+//  CoreFramework.h
+//  CoreFramework
+//
+//  Created by Kotchaphan Muangsan on 31/3/20.
+//  Copyright © 2020 Kotchaphan Muangsan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreFramework.
+FOUNDATION_EXPORT double CoreFrameworkVersionNumber;
+
+//! Project version string for CoreFramework.
+FOUNDATION_EXPORT const unsigned char CoreFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreFramework/PublicHeader.h>
+
+
